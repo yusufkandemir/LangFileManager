6ee788d9c6f165cd7c4b5a8cede7228f1a0bfb29
Ghitu <alin@updivision.com>
2016-04-29 05:43:28 -0400

    Applied fixes from StyleCI

--- src/app/Http/Requests/LanguageRequest.php
+++ src/app/Http/Requests/LanguageRequest.php
@@ -1,32 +1,33 @@
-<?php namespace Backpack\TranslationManager\app\Http\Requests;
+<?php
+
+namespace Backpack\TranslationManager\app\Http\Requests;
 
 use App\Http\Requests\Request;
 use Backpack\CRUD\app\Http\Requests\CrudRequest as CrudRequest;
 
-class LanguageRequest extends CrudRequest {
+class LanguageRequest extends CrudRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow creates if the user is logged in
+        return \Auth::check();
+    }
 
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		// only allow creates if the user is logged in
-		return \Auth::check();
-	}
-
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
-	{
-		return [
-			'name' => 'required|min:3|max:255',
-			'abbr' => 'required|min:2|max:2'
-		];
-	}
-
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:3|max:255',
+            'abbr' => 'required|min:2|max:2',
+        ];
+    }
 }
--- src/config/langfilemanager.php
+++ src/config/langfilemanager.php
@@ -2,9 +2,9 @@
 
 return [
 
-	// DO NOT ALLOW EDITS ON THESE LANGUAGE FILES
-	// Language files to NOT show in the LangFile Manager
-	//
-	'language_ignore' => ['pagination', 'reminders', 'validation', 'log', 'crud'],
+    // DO NOT ALLOW EDITS ON THESE LANGUAGE FILES
+    // Language files to NOT show in the LangFile Manager
+    //
+    'language_ignore' => ['pagination', 'reminders', 'validation', 'log', 'crud'],
 
 ];
