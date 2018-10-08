d18806e232695ab017f6d52d055e9af3fdade57b
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2018-10-08 22:32:55 +0300

    Removed authorize method
    We already have middlewares covering the routes, so adding this method like this is unnecessary. When it is not presents it authorized automatically if it passed middlewares.

--- src/app/Http/Requests/LanguageRequest.php
+++ src/app/Http/Requests/LanguageRequest.php
@@ -7,17 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class LanguageRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        // only allow creates if the user is logged in
-        return \Auth::check();
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
